610eacaafa3fc9df463294731a6eed2611740f4e
Tammie Chung <tammie.chung11@gmail.com>
2019-11-13 11:22:11 -0500

    Create form for adding new category

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -20,4 +20,12 @@ class Admin::CategoriesController < ApplicationController
     end
   end
 
+  private
+
+  def category_params
+    params.require(:category).permit(
+      :name
+    )
+  end
+
 end
