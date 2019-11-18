be20f0764cd42fb1047b453953cafc4921d4becc
Tammie Chung <tammie.chung11@gmail.com>
2019-11-18 11:54:00 -0500

    Create base validation - product with all attributes is valid

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,7 +2,12 @@ require 'rails_helper'
 
 RSpec.describe Product, type: :model do
   describe 'Validations' do
-    it 'is valid with valid attributes'
+    it 'is valid with valid attributes' do
+      category = Category.new
+      product = Product.new(name: 'a', price: 1, quantity: 1, category: category)
+      expect(product).to be_valid
+    end
+
     it 'is not valid without a name'
     it 'is not valid without a price'
     it 'is not valid without a quantity'
