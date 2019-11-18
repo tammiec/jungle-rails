1acefd102cd23b9e346966e1df323bc9c6e66b3a
Tammie Chung <tammie.chung11@gmail.com>
2019-11-18 12:49:50 -0500

    Implement name validation

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,7 +7,11 @@ RSpec.describe User, type: :model do
       expect(user).to be_valid
     end
 
-    it 'is not valid without a name'
+    it 'is not valid without a name' do
+      user = User.new(name: nil, email: 'a@a.com', password: 'abc', password_confirmation: 'abc')
+      expect(user).to_not be_valid
+    end
+
     it 'is not valid without an email'
     it 'is not valid if email is not unique'
     it 'is not valid without a password'
