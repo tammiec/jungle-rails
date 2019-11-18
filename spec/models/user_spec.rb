d0f45b409a21d1c5e66a6badc521d31d819cb10f
Tammie Chung <tammie.chung11@gmail.com>
2019-11-18 12:15:57 -0500

    Define validation specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Validations' do
+    it 'is valid with valid attributes'
+    it 'is not valid without a name'
+    it 'is not valid without an email'
+    it 'is not valid if email is not unique'
+    it 'is not valid without a password'
+    it 'is not valid without a password_confirmation'
+    it 'is not valid if password and password_confirmation do not match'
+  end
 end
