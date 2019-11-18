179869d0a5fd4e5ad3bb4313b0d312344d2ce490
Tammie Chung <tammie.chung11@gmail.com>
2019-11-18 11:56:41 -0500

    Implement no quantity validation

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -20,7 +20,12 @@ RSpec.describe Product, type: :model do
       expect(product).to_not be_valid
     end
     
-    it 'is not valid without a quantity'
+    it 'is not valid without a quantity' do
+      category = Category.new
+      product = Product.new(name: 'a', price: 1, quantity: nil, category: category)
+      expect(product).to_not be_valid
+    end
+
     it 'is not valid without a category'
   end
 end
