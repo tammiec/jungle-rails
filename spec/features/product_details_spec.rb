9e9f04f1868ef0e5dda588fa25d243963ace5c05
Tammie Chung <tammie.chung11@gmail.com>
2019-11-18 15:18:32 -0500

    Add feature spec for product page

---
+++ spec/features/product_details_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.feature "ProductDetails", type: :feature, js: true do
+    # SETUP
+    before :each do
+      @category = Category.create! name: 'Apparel'
+  
+      10.times do |n|
+        @category.products.create!(
+          name:  Faker::Hipster.sentence(3),
+          description: Faker::Hipster.paragraph(4),
+          image: open_asset('apparel1.jpg'),
+          quantity: 10,
+          price: 64.99
+        )
+      end
+      
+      visit root_path
+    end
+  
+    scenario "They see the product that is clicked" do
+      # ACT
+      find('article.product header', match: :first).click
+  
+      # DEBUG
+      save_screenshot
+  
+      # VERIFY
+      expect(page).to have_css 'article.product-detail'
+    end
+end
