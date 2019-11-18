5d97d14c935936a4cc46808a88469f5abd6a5af6
Tammie Chung <tammie.chung11@gmail.com>
2019-11-18 12:49:07 -0500

    Implement general validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
   has_secure_password
 
   validates :name, presence: true
-  validates :email, presence: true, uniqueness: { case_sensitive: false}
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
   validates :password, presence: true, confirmation: true
   validates :password_confirmation, presence: true
   
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,7 +2,11 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe 'Validations' do
-    it 'is valid with valid attributes'
+    it 'is valid with valid attributes' do
+      user = User.new(name: 'a', email: 'a@a.com', password: 'abc', password_confirmation: 'abc')
+      expect(user).to be_valid
+    end
+
     it 'is not valid without a name'
     it 'is not valid without an email'
     it 'is not valid if email is not unique'
