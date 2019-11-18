5400f6645dc77be47ef99b426d9f91337b9dc290
Tammie Chung <tammie.chung11@gmail.com>
2019-11-18 11:48:54 -0500

    Add validation statements

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,6 +2,10 @@ require 'rails_helper'
 
 RSpec.describe Product, type: :model do
   describe 'Validations' do
-    # validation tests/examples here
+    it 'is valid with valid attributes'
+    it 'is not valid without a name'
+    it 'is not valid without a price'
+    it 'is not valid without a quantity'
+    it 'is not valid without a category'
   end
 end
